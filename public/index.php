3dd035cfed02d83d6336aa9431dd99c5d1cd5ba0
mnippa <62095154+mnippa@users.noreply.github.com>
2026-01-28 15:31:25 +0100

    AMD Layout

--- public/index.php
+++ public/index.php
@@ -1,20 +1,33 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Python IDE</title>
-
-    <!-- Monaco CSS -->
-    <link rel="stylesheet" href="./monaco/min/vs/editor/editor.main.css">
-    <!-- kein import von standalone-tokens.css! -->
+  <meta charset="UTF-8">
+  <title>Monaco AMD IDE</title>
+  <style>
+    body, html { margin: 0; height: 100%; }
+    #editor { width: 100%; height: 100%; }
+  </style>
+  <!-- Monaco CSS -->
+  <link rel="stylesheet" href="./monaco/min/vs/editor/editor.main.css">
 </head>
-
 <body>
+  <div id="editor"></div>
 
-<div id="editor" style="width:100%;height:90vh;border:1px solid grey;"></div>
+  <!-- AMD Loader -->
+  <script src="./monaco/min/vs/loader.js"></script>
+  <script>
+    // Konfiguration für AMD
+    require.config({ paths: { vs: './monaco/min/vs' }});
 
-<!-- ESM Script -->
-<script type="module" src="./js/ide.js"></script>
-
+    require(['vs/editor/editor.main'], function() {
+      // Editor erstellen
+      monaco.editor.create(document.getElementById('editor'), {
+        value: `print("Hello, Monaco!")`,
+        language: 'python',       // Sprache
+        theme: 'vs-dark',         // Theme
+        automaticLayout: true     // Auto-Resize
+      });
+    });
+  </script>
 </body>
 </html>
